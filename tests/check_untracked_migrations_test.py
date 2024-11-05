324003432d1ed67bb8959fe8069a64b91a20d07c
Christian Clauss <cclauss@me.com>
2024-11-05 15:25:47 +0100

    pre-commit autoupdate 2024-08-08 (#15)

--- tests/check_untracked_migrations_test.py
+++ tests/check_untracked_migrations_test.py
@@ -4,15 +4,15 @@ from hooks.utils import get_current_branch
 
 def test_no_untracked_migrations(temp_git_dir):
     with temp_git_dir.as_cwd():
-        migrations_dir = temp_git_dir.mkdir('app')
-        migrations_dir.join('main.py').write("print('hello world')")
+        migrations_dir = temp_git_dir.mkdir("app")
+        migrations_dir.join("main.py").write("print('hello world')")
         assert main() == 0
 
 
 def test_untracked_migrations(temp_git_dir):
     with temp_git_dir.as_cwd():
-        migrations_dir = temp_git_dir.mkdir('app').mkdir('migrations')
-        migrations_dir.join('0001_initial.py').write("print('hello world')")
+        migrations_dir = temp_git_dir.mkdir("app").mkdir("migrations")
+        migrations_dir.join("0001_initial.py").write("print('hello world')")
         assert main() == 1
 
 
@@ -25,4 +25,3 @@ def test_running_on_correct_branch(temp_git_dir):
 def test_running_on_incorrect_branch(temp_git_dir):
     with temp_git_dir.as_cwd():
         assert main(["--branches", "branch_one", "branch_two"]) == 1
-
